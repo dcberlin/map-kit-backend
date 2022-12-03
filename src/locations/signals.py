ca203f0e7ffe0435035164b710c01fceef0b0a69
Theo Costea <theo.costea@gmail.com>
2022-12-03 12:05:27 +0100

    Send E-Mail when community is proposed

--- src/locations/signals.py
+++ src/locations/signals.py
@@ -1,3 +1,4 @@
+from django.core import mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -9,3 +10,10 @@ def notify_on_community_proposal(**kwargs):
     if kwargs.get("created") is True:
         instance = kwargs.get("instance")
         print(f"New community instance created: {instance!r}")
+        email = mail.EmailMessage(
+            subject=f"New community {instance.name} proposed!",
+            body="A new community has been proposed.",  # TODO: Phrase a proper body
+            from_email="from@example.com",  # TODO: Change to the real one
+            to=["to1@example.com"],  # TODO: Change to the real one
+        )
+        email.send()
