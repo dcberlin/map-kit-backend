06a7149b9aa5848c3964d99f5c50a273cd6cef16
Theo Costea <theo.costea@gmail.com>
2022-12-03 11:30:13 +0100

    Test notification for DCB admins on community proposals

--- src/tests/test_email.py
+++ src/tests/test_email.py
@@ -1,4 +1,9 @@
+from http import HTTPStatus
+
+import pytest
 from django.core import mail
+
+from locations.models import User
 
 
 def test_email_sanity(mailoutbox):
@@ -13,4 +18,22 @@ def test_email_sanity(mailoutbox):
     )
     email.send()
 
+    assert len(mailoutbox) == 1
+
+
+@pytest.fixture
+def user_unapproved():
+    user = User()
+    user.save()
+    return user
+
+
+@pytest.mark.django_db
+def test_community_creation(user_unapproved, mailoutbox, client):
+    client.force_login(user_unapproved)
+    response = client.post(
+        "/api/communities-admin/",
+        {"name": "Berlin", "slug": "berlin"},
+    )
+    assert response.status_code == HTTPStatus.CREATED
     assert len(mailoutbox) == 1
