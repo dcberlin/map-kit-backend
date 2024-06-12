ca4b4cbd6d4db4f2f42ed5a894853c03e77fa942
Theo Costea <theo.costea@gmail.com>
2024-06-12 21:04:53 +0200

    Set both Sentry profile and trace sampling to 100%

--- src/map_kit/settings.py
+++ src/map_kit/settings.py
@@ -33,10 +33,10 @@ if SENTRY_DSN := os.environ.get("SENTRY_DSN", None):
         integrations=[
             DjangoIntegration(),
         ],
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        # We recommend adjusting this value in production.
-        traces_sample_rate=0.01,
+        # We do tracing and profiling on 100% of transactions, this should
+        # work fine considering we have a low traffic application.
+        traces_sample_rate=1.0,
+        profiles_sample_rate=1.0,
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
         send_default_pii=True,
